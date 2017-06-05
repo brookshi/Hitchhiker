36d3dbb5ed3e50fad6b369bf2535e92d5fe22c24
brookshi <iwxiaot@gmail.com>
2017-06-05 22:25:55 +0800

    login panel

--- client/src/modules/login/index.tsx
+++ client/src/modules/login/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect, Dispatch } from 'react-redux';
-import { Row, Col } from 'antd';
+import { Row, Col, Form, Input, Icon, Button } from 'antd';
 import './style/index.less';
+
+const FormItem = Form.Item;
 
 interface LoginPanelStateProps { }
 
@@ -11,20 +13,66 @@ type LoginPanelProps = LoginPanelStateProps & LoginPanelDispatchProps;
 
 interface LoginPanelState { }
 
-class LoginPanel extends React.Component<LoginPanelProps, LoginPanelState> {
+class LoginPanel extends React.Component<LoginPanelProps & any, LoginPanelState> {
+
+    private handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    }
+
     public render() {
+        const { getFieldDecorator } = this.props.form;
         return (
             <div className="login-panel">
-                <Row style={{ height: '80%' }} type="flex" justify="center" align="middle">
-                    <Col span={12}>
-                        <div className="login-panel-desc-title">Api management for team</div>
-                        <div className="login-panel-desc-content">
-                            Hitchhiker is an <b><a target="blank" href="https://github.com/brookshi/hitchhiker">open source</a></b> Restful Api management platform. You can deploy it in your local server, then create your team, invite members to join, and create, maintain Api together.<br /><br />
-                            More useful feathers (Schedule, Document etc.) are coming soon.
-                    </div>
+                <Row style={{ height: '80%' }} type="flex" justify="center" align="middle" gutter={36}>
+                    <Col span={13}>
+                        <div style={{ float: 'right', maxWidth: 550 }}>
+                            <div className="login-panel-desc-title">Api integrated testing tool for team</div>
+                            <div className="login-panel-desc-content">
+                                Hitchhiker is an <b><a target="blank" href="https://github.com/brookshi/hitchhiker">open source</a></b> Restful Api integrated testing tool. You can deploy it in your local server. It make easier to manage Api with your team members.<br /><br />
+                                More useful feathers (Schedule, Document, Api Mock etc.) will come soon.
+                            </div>
+                        </div>
                     </Col>
-                    <Col span={7}>
-                        login panel
+                    <Col span={11}>
+                        <Form onSubmit={this.handleSubmit} className="login-panel-form">
+                            <FormItem>
+                                <div>
+                                    Email
+                                </div>
+                                {
+                                    getFieldDecorator('email', {
+                                        rules: [{ required: true, message: 'Please enter your email!' }],
+                                    })
+                                        (
+                                        <Input spellCheck={false} className="login-panel-form-input" prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Email" />
+                                        )
+                                }
+                            </FormItem>
+                            <FormItem>
+                                <div>
+                                    Password <a className="login-panel-form-forgot" href="">Forgot password?</a>
+                                </div>
+                                {
+                                    getFieldDecorator('password', {
+                                        rules: [{ required: true, message: 'Please enter your Password!' }],
+                                    })
+                                        (
+                                        <Input spellCheck={false} className="login-panel-form-input" prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password" />
+                                        )
+                                }
+                            </FormItem>
+                            <FormItem>
+                                <Button type="primary" htmlType="submit" className="login-panel-form-button">
+                                    Sign in
+                                </Button>
+                                New to Hitchhier? <a href="">Create an account.</a>
+                            </FormItem>
+                        </Form>
                     </Col>
                 </Row>
             </div>
@@ -44,7 +92,9 @@ const mapDispatchToProps = (dispatch: Dispatch<any>): LoginPanelDispatchProps =>
     };
 };
 
+const WrappedNormalLoginForm = Form.create()(LoginPanel);
+
 export default connect(
     mapStateToProps,
     mapDispatchToProps,
-)(LoginPanel);
+)(WrappedNormalLoginForm);
