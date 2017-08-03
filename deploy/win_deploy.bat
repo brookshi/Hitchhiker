2877415f2fb48e7f0f8c16d0da185100c4762b24
brookshi <iwxiaot@gmail.com>
2017-08-03 13:52:14 +0800

    add pm2.json

---
+++ deploy/win_deploy.bat
@@ -0,0 +1,18 @@
+set host=10.86.18.215
+
+git clone -b release https://github.com/brookshi/Hitchhiker.git
+cd hitchhiker
+call npm install
+cd client
+call npm install
+cd..
+call gulp release --prod
+
+@echo off&setlocal enabledelayedexpansion
+for /f "eol=* tokens=*" %%i in (pm2.json) do (
+set a=%%i
+set "a=!a:$host=%host%!"
+echo !a!>>$)
+move $ pm2.json
+
+pm2 start pm2.json --env production
