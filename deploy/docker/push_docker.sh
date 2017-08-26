bf52ea57d939b2d3d83358c41b1886fcc779ebbc
brookshi <iwxiaot@gmail.com>
2017-08-26 15:16:46 +0800

    update docker file

--- deploy/docker/push_docker.sh
+++ deploy/docker/push_docker.sh
@@ -1,4 +1,4 @@
-export HITCHHIKER_VERSION="v0.1.0"
+export HITCHHIKER_VERSION="v0.1.1"
 sudo docker pull registry.us-east-1.aliyuncs.com/brook/hitchhiker:$HITCHHIKER_VERSION
 sudo docker login --username=brookshi --password=$DOCKER_ID_PWD
 sudo docker tag registry.us-east-1.aliyuncs.com/brook/hitchhiker:$HITCHHIKER_VERSION brookshi/hitchhiker:$HITCHHIKER_VERSION
