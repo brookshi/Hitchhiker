873da74cfe578ce94ec613d4b3dfcbd823573441
brookshi <iwxiaot@gmail.com>
2017-12-25 00:10:31 +0800

    code snippet

---
+++ client/src/common/code_snippet_type.ts
@@ -0,0 +1,16 @@
+export const CodeSnippetType = {
+    c: { name: 'C (LibCurl)', types: ['libcurl'] },
+    clojure: { name: 'Clojure (CljHttp)', types: ['clj-http'] },
+    'c#': { name: 'C# (RestSharp)', types: ['restsharp'] },
+    go: { name: 'Go', types: ['native'] },
+    java: { name: 'Java', types: ['okhttp', 'unirest'] },
+    javascript: { name: 'Javascript', types: ['jquery', 'xhr'] },
+    node: { name: 'Node', types: ['native', 'request', 'unirest'] },
+    objc: { name: 'Objective-C (NSURL)', types: ['nsurlsession'] },
+    ocaml: { name: 'OCaml (Cohttp)', types: ['cohttp'] },
+    php: { name: 'PHP', types: ['curl', 'http1', 'http2'] },
+    python: { name: 'Python', types: ['request', 'python3'] },
+    ruby: { name: 'Ruby', types: ['native'] },
+    shell: { name: 'Shell', types: ['curl', 'httpie', 'wget'] },
+    swift: { name: 'Swift (NSURL)', types: ['nsurlsession'] }
+};
