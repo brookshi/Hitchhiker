01c0b49e5a1e6e416a49b3e4945ea34422e29705
brookshi <iwxiaot@gmail.com>
2018-03-05 17:35:22 +0800

    #103 improve email format checking in client

--- client/src/utils/string_util.ts
+++ client/src/utils/string_util.ts
@@ -103,7 +103,7 @@ export class StringUtil {
     }
 
     static checkEmail(email: string): boolean {
-        const pattern = /^[^@]+@[^\.@]+\.[a-zA-Z]+$/;
+        const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return pattern.test(email);
     }
 
