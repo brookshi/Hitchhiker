4bed4463b343f3c986a4b39dd3ef8972a6acbe0d
brookshi <iwxiaot@gmail.com>
2018-08-01 17:58:13 +0800

    refactor webpack config

--- client/config/webpack.config.dev.js
+++ client/config/webpack.config.dev.js
@@ -1,133 +1,103 @@
 'use strict';
 
-var autoprefixer = require('autoprefixer');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
-//var InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
-// var WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin');
-var getClientEnvironment = require('./env');
-var paths = require('./paths');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack');
+const getClientEnvironment = require('./env');
+const paths = require('./paths');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+//const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 
-// Webpack uses `publicPath` to determine where the app is being served from.
-// In development, we always serve from the root. This makes config easier.
-var publicPath = '/';
-// `publicUrl` is just like `publicPath`, but we will provide it to our app
-// as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
-// Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
-var publicUrl = '';
-// Get environment variables to inject into our app.
-var env = getClientEnvironment(publicUrl);
+const publicPath = '/';
+const publicUrl = '';
+const env = getClientEnvironment(publicUrl);
+const theme = require(paths.appPackageJson).theme;
 
-var cssFilename = 'static/css/[name].[contenthash:8].css';
-
-var theme = require(paths.appPackageJson).theme;
-
-
-var commonCssETP = new ExtractTextPlugin('static/css/common.css', {
+const commonCssETP = new ExtractTextPlugin('static/css/common.css', {
+  disable: false,
+  allChunks: true,
+});
+const commonLessETP = new ExtractTextPlugin('static/css/common.css', {
+  disable: false,
+  allChunks: true,
+});
+const appETP = new ExtractTextPlugin('static/css/app.css', {
   disable: false,
   allChunks: true,
 });
 
-var commonLessETP = new ExtractTextPlugin('static/css/common.css', {
-  disable: false,
-  allChunks: true,
-});
+const cssLoader = {
+  loader: 'css-loader',
+  options: {
+    sourceMap: true,
+    // modules: true
+  }
+};
+const postcssLoader = {
+  loader: 'postcss-loader',
+  options: {
+    sourceMap: true,
+    options: {
+      config: {
+        path: './'
+      }
+    }
+  }
+};
 
-var appETP = new ExtractTextPlugin('static/css/app.css', {
-  disable: false,
-  allChunks: true,
-});
+const appLessLoader = {
+  loader: 'less-loader',
+  options: {
+    strictMath: true,
+    javascriptEnabled: true
+  }
+};
 
-// This is the development configuration.
-// It is focused on developer experience and fast rebuilds.
-// The production configuration is different and lives in a separate file.
+const commonLessLoader = {
+  loader: 'less-loader',
+  options: {
+    javascriptEnabled: true,
+    modifyVars: theme
+  }
+};
+
+const getUrlLoader = (minetype) => {
+  const loader = {
+    loader: 'url-loader',
+    options: {
+      limit: 10000,
+      name: 'static/fonts/[name].[ext]'
+    }
+  };
+  if (minetype) {
+    loader.options.minetype = minetype;
+  }
+  return loader;
+}
+
 module.exports = {
-  // You may want 'eval' instead if you prefer to see the compiled output in DevTools.
-  // See the discussion in https://github.com/facebookincubator/create-react-app/issues/343.
   devtool: 'cheap-module-source-map',
-  // These are the "entry points" to our application.
-  // This means they will be the "root" imports that are included in JS bundle.
-  // The first two entry points enable "hot" CSS and auto-refreshes for JS.
   entry: [
-    // Include an alternative client for WebpackDevServer. A client's job is to
-    // connect to WebpackDevServer by a socket and get notified about changes.
-    // When you save a file, the client will either apply hot updates (in case
-    // of CSS changes), or refresh the page (in case of JS changes). When you
-    // make a syntax error, this client will display a syntax error overlay.
-    // Note: instead of the default WebpackDevServer client, we use a custom one
-    // to bring better experience for Create React App users. You can replace
-    // the line below with these two lines if you prefer the stock client:
-    // require.resolve('webpack-dev-server/client') + '?/',
-    // require.resolve('webpack/hot/dev-server'),
     require.resolve('react-dev-utils/webpackHotDevClient'),
-    // We ship a few polyfills by default:
     require.resolve('./polyfills'),
-    // Finally, this is your app's code:
     paths.appIndexJs
-    // We include the app code last so that if there is a runtime error during
-    // initialization, it doesn't blow up the WebpackDevServer client, and
-    // changing JS code would still trigger a refresh.
   ],
   output: {
-    // Next line is not used in dev but WebpackDevServer crashes without it:
     path: paths.appBuild,
-    // Add /* filename */ comments to generated require()s in the output.
     pathinfo: true,
-    // This does not produce a real file. It's just the virtual path that is
-    // served by WebpackDevServer in development. This is the JS bundle
-    // containing code from all our entry points, and the Webpack runtime.
     filename: 'static/js/bundle.js',
-    // This is the URL that app is served from. We use "/" in development.
     publicPath: publicPath
   },
   resolve: {
-    // This allows you to set a fallback for where Webpack should look for modules.
-    // We read `NODE_PATH` environment variable in `paths.js` and pass paths here.
-    // We use `fallback` instead of `root` because we want `node_modules` to "win"
-    // if there any conflicts. This matches Node resolution mechanism.
-    // https://github.com/facebookincubator/create-react-app/issues/253
-    //fallback: paths.nodePaths,
-    // These are the reasonable defaults supported by the Node ecosystem.
-    // We also include JSX as a common component filename extension to support
-    // some tools, although we do not recommend using it, see:
-    // https://github.com/facebookincubator/create-react-app/issues/290
     extensions: ['.ts', '.tsx', '.js', '.json', '.jsx'],
     alias: {
-      // Support React Native Web
-      // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web'
     }
   },
   mode: 'development',
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        styles: {
-          name: 'styles',
-          test: /\.css$/,
-          chunks: 'all',
-          enforce: true
-        }
-      }
-    }
-  },
   module: {
-    // First, run the linter.
-    // It's important to do this before Babel processes the JS.
     noParse: /[\/\\]node_modules[\/\\]localforage[\/\\]dist[\/\\]localforage\.js$/,
     rules: [
-      // ** ADDING/UPDATING LOADERS **
-      // The "url" loader handles all assets unless explicitly excluded.
-      // The `exclude` list *must* be updated with every change to loader extensions.
-      // When adding a new loader, you must add its `test`
-      // as a new entry in the `exclude` list for "url" loader.
-
-      // "url" loader embeds assets smaller than specified size as data URLs to avoid requests.
-      // Otherwise, it acts like the "file" loader.
       // {
       //   test: /\.(ts|tsx)$/,
       //   use: [{
@@ -141,12 +111,6 @@ module.exports = {
       {
         exclude: [
           /\.html$/,
-          // We have to write /\.(js|jsx)(\?.*)?$/ rather than just /\.(js|jsx)$/
-          // because you might change the hot reloading server from the custom one
-          // to Webpack's built-in webpack-dev-server/client?/, which would not
-          // get properly excluded by /\.(js|jsx)$/ because of the query string.
-          // Webpack 2 fixes this, but for now we include this hack.
-          // https://github.com/facebookincubator/create-react-app/issues/1713
           /\.(js|jsx)(\?.*)?$/,
           /\.(ts|tsx)(\?.*)?$/,
           /\.less$/,
@@ -158,63 +122,24 @@ module.exports = {
           /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
           /\.eot(\?v=\d+\.\d+\.\d+)?$/,
         ],
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            name: `static/media/[name].[ext]`
-          }
-        }],
+        use: [getUrlLoader()],
       },
-      // Compile .tsx?
       {
         test: /\.(ts|tsx)$/,
         include: paths.appSrc,
-        use: ['babel-loader', {
-          loader: 'awesome-typescript-loader'
-        }]
+        use: [
+          'babel-loader',
+          'awesome-typescript-loader'
+        ]
       },
-      // "postcss" loader applies autoprefixer to our CSS.
-      // "css" loader resolves paths in CSS and adds assets as dependencies.
-      // "style" loader turns CSS into JS modules that inject <style> tags.
-      // In production, we use a plugin to extract that CSS to a file, but
-      // in development "style" loader enables hot editing of CSS.
-      // {
-      //   test: /\.css$/,
-      //   loader: [{
-      //     loader: 'css-loader',
-      //     options: {
-      //       importLoaders: 1
-      //     }
-      //   }, {
-      //     loader: 'postcss-loader',
-      //     options: {
-      //       sourceMap: true,
-      //       plugins: function () {
-      //         return [];
-      //       }
-      //     }
-      //   }]
-      // },
       {
         test: /\.css$/,
         use: commonCssETP.extract({
           fallback: 'style-loader',
-          use: [{
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-              // modules: true
-            }
-          }, {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true,
-              plugins: function () {
-                return [];
-              }
-            }
-          }]
+          use: [
+            cssLoader,
+            postcssLoader
+          ]
         })
       },
       {
@@ -222,29 +147,10 @@ module.exports = {
         exclude: paths.appSrc,
         use: commonLessETP.extract({
           fallback: 'style-loader',
-          use: [{
-              loader: 'css-loader',
-              options: {
-                sourceMap: true,
-                // modules: true
-              }
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                sourceMap: true,
-                plugins: function () {
-                  return [];
-                }
-              }
-            },
-            {
-              loader: 'less-loader',
-              options: {
-                javascriptEnabled: true,
-                modifyVars: theme
-              }
-            }
+          use: [
+            cssLoader,
+            postcssLoader,
+            commonLessLoader
           ]
         })
       },
@@ -253,64 +159,13 @@ module.exports = {
         include: paths.appSrc,
         use: appETP.extract({
           fallback: 'style-loader',
-          use: [{
-              loader: 'css-loader',
-              options: {
-                sourceMap: true,
-                // modules: true
-              }
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                sourceMap: true,
-                plugins: function () {
-                  return [];
-                }
-              }
-            },
-            {
-              loader: 'less-loader',
-              options: {
-                strictMath: true,
-                javascriptEnabled: true
-              }
-            }
+          use: [
+            cssLoader,
+            postcssLoader,
+            appLessLoader
           ]
         })
       },
-      // {
-      //   test: /\.less$/,
-      //   exclude: paths.appSrc,
-      //   use: ExtractTextPlugin.extract([
-      //     // 'css-loader',
-      //     // 'less-loader',
-      //     // 'postcss-loader'
-      //     {
-      //       loader: 'css-loader',
-      //       options: {
-      //         sourceMap: true,
-      //         modules: true
-      //       }
-      //     }, {
-      //       loader: 'postcss-loader',
-      //       options: {
-      //         sourceMap: true,
-      //         plugins: function () {
-      //           return [];
-      //         }
-      //       }
-      //     }, {
-      //       loader: 'less-loader',
-      //       options: {
-      //         sourceMap: true,
-      //         modifyVars: JSON.stringify(theme)
-      //       }
-      //     }
-      //   ])
-      // },
-      // // JSON is not enabled by default in Webpack but both Node and Browserify
-      // // allow it implicitly so we also enable it.
       // {
       //   test: /\.json$/,
       //   use: 'json-loader'
@@ -318,81 +173,23 @@ module.exports = {
       // "file" loader for svg
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            minetype: `image/svg+xml`
-          }
-        }]
+        use: [getUrlLoader('image/svg+xml')]
       },
       {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            name: 'static/fonts/[name].[ext]',
-            minetype: `application/font-woff`
-          }
-        }]
-      },
-      {
-        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            name: 'static/fonts/[name].[ext]',
-            minetype: `application/font-woff`
-          }
-        }]
+        test: /\.woff2?(\?v=\d+\.\d+\.\d+)?$/,
+        use: [getUrlLoader('application/font-woff')]
       },
       {
         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            name: 'static/fonts/[name].[ext]',
-            minetype: `application/octet-stream`
-          }
-        }]
+        use: [getUrlLoader('application/octet-stream')]
       },
       {
         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            name: 'static/fonts/[name].[ext]',
-            minetype: `application/vnd.ms-fontobject`
-          }
-        }]
+        use: [getUrlLoader('application/vnd.ms-fontobject')]
       }
-      // ** STOP ** Are you adding a new loader?
-      // Remember to add the new extension(s) to the "url" loader exclusion list.
     ]
   },
-  // We use PostCSS for autoprefixing only.
-  // postcss: function () {
-  //   return [
-  //     autoprefixer({
-  //       browsers: [
-  //         '>1%',
-  //         'last 4 versions',
-  //         'Firefox ESR',
-  //         'not ie < 9', // React doesn't support IE8 anyway
-  //       ]
-  //     }),
-  //   ];
-  // },
   plugins: [
-    // Makes some environment variables available in index.html.
-    // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
-    // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
-    // In development, this will be an empty string.
-    // Generates an `index.html` file with the <script> injected.
     new HtmlWebpackPlugin({
       inject: true,
       template: paths.appHtml,
@@ -401,28 +198,10 @@ module.exports = {
     commonCssETP,
     commonLessETP,
     appETP,
-    // new MiniCssExtractPlugin({
-    //   filename: "static/css/[name].css",
-    //   chunkFilename: "static/css/[id].css"
-    // }),
-    // Makes some environment variables available to the JS code, for example:
-    // if (process.env.NODE_ENV === 'development') { ... }. See `./env.js`.
-    // new webpack.DefinePlugin(env.stringified),
-    // This is necessary to emit hot updates (currently CSS only):
     new webpack.HotModuleReplacementPlugin(),
-    // Watcher doesn't work well if you mistype casing in a path so we use
-    // a plugin that prints an error when you attempt to do this.
-    // See https://github.com/facebookincubator/create-react-app/issues/240
     new CaseSensitivePathsPlugin(),
-    // If you require a missing module and then `npm install` it, you still have
-    // to restart the development server for Webpack to discover it. This plugin
-    // makes the discovery automatic so you don't have to restart.
-    // See https://github.com/facebookincubator/create-react-app/issues/186
-    // new WatchMissingNodeModulesPlugin(paths.appNodeModules),
     //new BundleAnalyzerPlugin()
   ],
-  // Some libraries import Node modules but don't use them in the browser.
-  // Tell Webpack to provide empty mocks for them so importing them works.
   node: {
     fs: 'empty',
     net: 'empty',
